bc256b0aa3728255cd2f925e4c55f769b01e4a32
Marcus Jensen <marcusproman0507@gmail.com>
2020-11-04 15:03:07 +0100

    Updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace UdlaansSystem
 {
@@ -128,6 +129,53 @@ namespace UdlaansSystem
             return cardUID;
         }
         #endregion
+        #region WindowUI
+        /// <summary>
+        /// Minimize
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Button_Click_Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        ///// <summary>
+        ///// Resize
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public void Button_Click_Resize(object sender, RoutedEventArgs e)
+        //{
+        //    if (WindowState.Equals(WindowState.Normal))
+        //    {
+        //        WindowState = WindowState.Maximized;
+        //        resizeButton.Content = ">";
+        //    }
+        //    else
+        //    {
+        //        WindowState = WindowState.Normal;
+        //        resizeButton.Content = "<";
+        //    }
+        //}
+        /// <summary>
+        /// Close
+        /// </summary>
+        /// <param name = "sender" ></ param >
+        /// < param name="e"></param>
+        public void Button_Click_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Drag header
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void WindowHeader_Mousedown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        #endregion
         public MainWindow()
         {
             InitializeComponent();
@@ -135,11 +183,11 @@ namespace UdlaansSystem
             establishContext();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ButtonClickGetCardUID(object sender, RoutedEventArgs e)
         {
             if (connectCard())
             {
-                MessageBox.Show(getcardUID());
+                CardReaderInput.Text = getcardUID();
             }
         }
     }
