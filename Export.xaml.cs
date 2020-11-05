7e76fdedfd48377d53f8bf56c573edd2e7bab435
Joachim <joac3146@zbc.dk>
2020-11-05 13:41:07 +0100

    Changes made

--- Export.xaml.cs
+++ Export.xaml.cs
@@ -25,28 +25,59 @@ namespace UdlaansSystem
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void QRScannerInput_KeyDown(object sender, KeyEventArgs e)
+        /// 
+
+
+
+        private void QRScannerUniLogin_KeyDown(object sender, KeyEventArgs e)
         {
+            string loanerQR = "udlaan" + UniLoginInput.Text;
+
             if (e.Key == Key.Enter)
             {
                 try
                 {
-                    QRScannerInput.Text = "Write some data: ";
+                    QRScannerUniLogin.Text = "Write some data: ";
                     WebClient client = new WebClient();
-                    client.DownloadFile($@"https://api.qrserver.com/v1/create-qr-code/?color=255-0-0&bgcolor=255-255-255&format=png&data={QRScannerInput.Text}", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\example.png");
+                    client.DownloadFile($@"https://api.qrserver.com/v1/create-qr-code/?color=255-0-0&bgcolor=255-255-255&format=png&data={loanerQR}", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\example.png");
                 }
                 catch (Exception)
                 { }
                 if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\example.png"))
                 {
-                    QRScannerInput.Text = "Success";
+                    QRScannerUniLogin.Text = "Success";
                     Process.Start($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\example.png");
                 }
                 else
-                    QRScannerInput.Text = "Failed";
+                    QRScannerUniLogin.Text = "Failed";
             }
-
         }
 
+        private void QRScannerSerialNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            string dummySerial = "W5tR213lIm";
+
+            string computerQR = "SkpRiIt" + dummySerial;
+
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    QRScannerSerialNumber.Text = "Write some data: ";
+                    WebClient client = new WebClient();
+                    client.DownloadFile($@"https://api.qrserver.com/v1/create-qr-code/?color=255-0-0&bgcolor=255-255-255&format=png&data={computerQR}", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\exampleSerial.png");
+                }
+                catch (Exception)
+                { }
+                if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\exampleSerial.png"))
+                {
+                    // QRScannerSerialNumber.Text = "Success";
+                    QRScannerSerialNumber.Text = dummySerial;
+                    Process.Start($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\exampleSerial.png");
+                }
+                else
+                    QRScannerSerialNumber.Text = "Failed";
+            }
+        }
     }
 }
