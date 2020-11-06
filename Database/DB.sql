7dd0f8310af02ce8d674ecd1b634229927d596cb
Marcus Bournonwille <Marc8847@zbc.dk>
2020-11-06 13:15:40 +0100

    Create DB.sql

---
+++ Database/DB.sql
@@ -0,0 +1,77 @@
+DROP table onetimeloan;
+DROP table udlaant;
+DROP table pc;
+Drop Table laaner;
+
+CREATE TABLE laaner(
+	login varchar(16) primary key,
+	laanerID varchar(16),
+	name varchar(255),
+	tlf_Number varchar(8),
+	is_student bool
+);
+
+CREATE TABLE pc(
+	QR_ID varchar(16) primary Key,
+	loebe_nummer varchar(16),
+	pc_model varchar(255),
+	in_stock bool
+);
+
+CREATE TABLE udlaant(
+	login varchar(16) references laaner(login),
+	QR_ID varchar(16) references pc(QR_ID),
+	start_date date,
+	end_date date
+);
+CREATE TABLE OneTimeLoan(
+	phone_number varchar(8) primary key,
+	qr_id varchar(5) references pc(QR_ID),
+	date_of_loan date
+);
+
+CREATE or REPLACE PROCEDURE tlf_loan(
+	tlf_number varchar(8),
+	qr_id varchar(16),
+	todays_date date
+)
+language plpgsql as $$
+begin
+	INSERT INTO OneTimeLoan VALUES (tlf_number, qr_id, todays_date);
+	UPDATE pc SET in_stock = false WHERE QR_ID = qr_id;
+	commit;
+end;$$;
+
+CREATE or REPLACE PROCEDURE tlf_return(
+	qr_id Varchar(16)
+)
+Language plpgsql as $$
+begin
+	Delete from OneTimeLoan Where QR_id = qr_id;
+	Update pc set in_stock = true Where QR_ID = qr_id;
+	commit;
+end;$$;
+
+CREATE or REPLACE PROCEDURE _Udlaan(
+	laanerID varchar(16),
+	qr_id varchar(16),
+	start_date date,
+	end_date date
+)
+language plpgsql as $$
+begin
+	insert into Udlaant Values (laanerID, qr_id, start_date, end_date);
+	Update pc set in_stock = false Where QR_ID = qr_id;
+	commit;
+end;$$;
+
+CREATE or REPLACE PROCEDURE _aflever(
+	laanerID varchar(16),
+	qr_id varchar(16)
+)
+language plpgsql as $$
+begin
+	Delete From laaner where QR_ID = qr_id;
+	Update pc set in_stock = true Where QR_ID = qr_id;
+	commit;
+end;$$;
