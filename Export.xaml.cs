51c9d412289e675fba06c845fc18409dc5a51701
Joachim <joac3146@zbc.dk>
2020-11-18 12:40:01 +0100

    Jaja

---
+++ Export.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+
+namespace UdlaansSystem
+{
+    public partial class Export : Page
+    {
+        /// <summary>
+        /// Main
+        /// </summary>
+        public Export()
+        {
+            InitializeComponent();
+            CreateFolders();
+
+        }
+        /// <summary>
+        /// Scan card click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+
+
+        public void CreateFolders()
+        {
+            // Specify the directory you want to manipulate.
+            string folderPathPC = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesPC";
+            string folderPathLoaner = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesLoaner";
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(folderPathPC))
+                {
+                    return;
+                }
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(folderPathPC);
+            }
+            catch { }
+            finally { }
+
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(folderPathLoaner))
+                {
+                    return;
+                }
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(folderPathLoaner);
+            }
+            catch { }
+            finally { }
+        }
+
+
+        private void QRScannerUniLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            string loanerQR = "udlaan" + UniLoginInput.Text;
+
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    QRScannerUniLogin.Text = "Write some data: ";
+                    WebClient client = new WebClient();
+                    client.DownloadFile($@"https://api.qrserver.com/v1/create-qr-code/?color=255-0-0&bgcolor=255-255-255&format=png&data={loanerQR}", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesLoaner\{loanerQR}.png");
+                }
+                catch (Exception)
+                { }
+                if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesLoaner\{loanerQR}.png"))
+                {
+                    QRScannerUniLogin.Text = "Success";
+                    Process.Start($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesLoaner\{loanerQR}.png");
+                }
+                else
+                    QRScannerUniLogin.Text = "Failed";
+            }
+        }
+
+        private void QRScannerSerialNumber_KeyDown(object sender, KeyEventArgs e)
+        {
+            string dummySerial = "W5tR213lIm";
+
+            string computerQR = "SkpRiIt" + dummySerial;
+
+            if (e.Key == Key.Enter)
+            {
+                try
+                {
+                    QRScannerSerialNumber.Text = "Write some data: ";
+                    WebClient client = new WebClient();
+                    client.DownloadFile($@"https://api.qrserver.com/v1/create-qr-code/?color=255-0-0&bgcolor=255-255-255&format=png&data={computerQR}", $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesPC\{computerQR}.png");
+                }
+                catch (Exception)
+                { }
+                if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesPC\{computerQR}.png"))
+                {
+                    // QRScannerSerialNumber.Text = "Success";
+                    QRScannerSerialNumber.Text = dummySerial;
+                    Process.Start($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\QRcodesPC\{computerQR}.png");
+                }
+                else
+                    QRScannerSerialNumber.Text = "Failed";
+            }
+        }
+
+        private void DateInput_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true; 
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                ContextMenu cm = (ContextMenu)Resources["cmCalendar"];
+                cm.IsOpen = true;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace UdlaansSystem
+{
+    public partial class MainWindow : Window
+    {
+        #region WindowUI
+        /// <summary>
+        /// Minimize
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Button_Click_Minimize(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        /// <summary>
+        /// Resize
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Button_Click_Resize(object sender, RoutedEventArgs e)
+        {
+            if (WindowState.Equals(WindowState.Normal))
+            {
+                WindowState = WindowState.Maximized;
+                resizeButton.Content = ">";
+            }
+            else
+            {
+                WindowState = WindowState.Normal;
+                resizeButton.Content = "<";
+            }
+        }
+        /// <summary>
+        /// Close
+        /// </summary>
+        /// <param name = "sender" ></ param >
+        /// < param name="e"></param>
+        public void Button_Click_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        /// <summary>
+        /// Drag header
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void WindowHeader_Mousedown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        /// <summary>
+        /// Export page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = new Export();
+        }
+        /// <summary>
+        /// Import page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = new Import();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            Display.Content = new Register();
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
